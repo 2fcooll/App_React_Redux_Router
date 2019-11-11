3d6475a5402d739cf9c33f08b1988e2066f738ed
Maksim Mogachev <maksim.1997mogachev@gmail.com>
2019-11-11 20:51:07 +0300

    config

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Profile = ({ user }) => {
-  if (user) {
+  console.log(user, 'user');
+  if (Object.keys(user).length) {
     return (
-      <div>{user.first_name}</div>
+      <div className='profile'>
+        <div>
+          <img className='profile__image' src={user._links.avatar.href} />
+        </div>
+        <div>
+          <ul className='profile__list'>
+            <li className='profile__list-item'>{`${user.first_name} ${user.last_name}`}</li>
+            <li className='profile__list-item'>{user.address}</li>
+            <li className='profile__list-item'>{user.email}</li>
+            <li className='profile__list-item'>{user.phone}</li>
+          </ul>
+        </div>
+      </div>
     );
   }
   return null;
